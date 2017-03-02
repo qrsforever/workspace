8e21b181e565f2a2b303aa2f16a2252df1ff21d5
Li Dong <lidong8@le.com>
2017-03-02 12:49:15 +0800

    Python: Add mods install shell

---
+++ python/m_install.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+sudo apt-get install python
+
+# 基础开发环境
+sudo apt-get install python-dev
+
+# 管理工具
+sudo apt-get install python-pip
+
+# 增强Shell
+sudo apt-get install ipython
+
+# 安装Notebook以及依赖库:tornado 和 pyzmq.
+sudo apt-get install ipython-notebook 
+sudo pip install tornado
+sudo apt-get install libzmq-dev
+sudo pip install pyzmq
+sudo pip install pygments
+
+# 科学计算相关库(矩阵，数值计算， 绘图)
+sudo apt-get install python-numpy
+sudo apt-get install python-scipy
+sudo apt-get install python-matplotlib
+
+# 使用pip编译安装时， 检查依赖, 更新
+sudo apt-get build-dep python-numpy
+sudo apt-get build-dep python-scipy
+sudo pip install numpy
+sudo pip install scipy
+
+# Qt设计器
+sudo apt-get install python-qt4
+sudo apt-get install qt4-designer
+sudo apt-get install pyqt4-dev-tools
+sudo apt-get install python-qt4-doc
+
+# 安装简单IDE 
+sudo apt-get install spyder
+sudo pip install spyder --upgrade
+
+# 扩展模块
+sudo pip install cython
+sudo apt-get install swig
+
+# 数据分析
+sudo apt-get python-pandas
+
+# 工具套装ETS
+sudo apt-get install libxtst-dev scons python-vtk pyqt4-dev-tools 
+sudo apt-get install python2.7-wxgtk2.8 python-configobj
+sudo apt-get install libgl1-mesa-dev libglu1-mesa-dev
+rm -f /tmp/ets
+mkdir /tmp/ets
+cd /tmp/ets
+git clone git@github.com:enthought/ets.git
+cd ets
+python ets.py clone
+sudo python setup.py develop
+cd -
+cd -
+
+# 图像处理和计算机视觉 (opencv)
+sudo apt-get install harpia libcv2.4 libcvaux2.4 
+sudo apt-get install libhighgui2.4 libcv-dev libcvaux-dev
+sudo apt-get install libhighgui-dev opencv-doc python-opencv
+
+# 测试库
+sudo apt-get install python-nose
