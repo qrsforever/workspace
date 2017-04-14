8d9c5bfe9331c290d31a0f64e1a4cb28736d0092
Li Dong <lidong8@le.com>
2017-04-14 11:46:57 +0800

    Android: Test: Add test files

--- android/test/performance/traceview/src/com/leeco/test/performance/TraceViewDemoActivity.java
+++ android/test/performance/traceview/src/com/leeco/test/performance/TraceViewDemoActivity.java
@@ -1,14 +1,20 @@
 package com.leeco.test.performance;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Debug;
 import android.os.Environment;
+import android.os.Handler;
 import android.os.SystemClock;
 import android.util.Log;
 
 public class TraceViewDemoActivity extends Activity {
     public static final String TAG = TraceViewDemoActivity.class.getSimpleName();
+
+    private Handler mH = new Handler();
 
     public TraceViewDemoActivity() {
         super();
@@ -24,6 +30,7 @@ public class TraceViewDemoActivity extends Activity {
         Log.d(TAG, "onCreate");
         setContentView(R.layout.main);
         new Thread(new Runnable() {
+
             @Override
             public void run() {
                 int i = 0;
@@ -36,6 +43,34 @@ public class TraceViewDemoActivity extends Activity {
         for (int i = 0; i < 500; i++) {
             myLog("i = " + i);
         }
+
+        mH.post(new Runnable() {
+
+            @Override
+            public void run() {
+                Log.d(TAG, "On main thread run");
+                InputStream in = null;
+                byte[] tempbytes = new byte[1024];  
+                int byteread = 0;
+                try {
+                    in = new FileInputStream(Environment.
+                            getExternalStorageDirectory().getPath() + "/test.bin");  
+                    while ((byteread = in.read(tempbytes)) != -1) {
+                        myLog("read [" + byteread + "]");
+                    }
+                } catch(Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (in != null) {
+                        try {
+                            in.close();
+                        } catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        });
     }
 
     private void myLog(String txt) {
@@ -56,3 +91,7 @@ public class TraceViewDemoActivity extends Activity {
         Debug.stopMethodTracing();
     }
 }
+/*
+I find a solution about it. Just replace the sdk_home/tools/lib/monitor-x86/plugins/com.android.ide.eclipse.traceview_24.3.3.2016344.jar  with   sdk_home/tools/lib/traveview.jar .Note that rename the file name at the same time.
+ * 
+ */
