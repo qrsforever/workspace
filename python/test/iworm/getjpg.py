6c970b60c9f2a2315ca5a33b87d33bce488b39a7
Li Dong <lidong8@le.com>
2017-04-14 14:17:49 +0800

    Python: Test: 解析Android日志绘制应用启动耗时图

---
+++ python/test/iworm/getjpg.py
@@ -0,0 +1,23 @@
+#coding=utf-8
+import urllib
+import re
+
+# def getHtml(url):
+    # page = urllib.urlopen(url)
+    # html = page.read()
+    # return html
+
+# def getImg(html):
+    # reg = r'href="(http*)" data-ga-click'
+    # imgre = re.compile(reg)
+    # imglist = re.findall(imgre,html)
+    # x = 0
+
+    # for imgurl in imglist:
+        # print(imgurl)
+        # # urllib.urlretrieve(imgurl,'%s.jpg' % x)
+        # x+=1
+
+# html = getHtml("https://github.com/qrsforever")
+
+# # print(getImg(html))
