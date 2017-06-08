5f6a696742d8151a1efa94db97de85a10e5b3a87
Dong Li <985612771@qq.com>
2017-06-08 08:20:42 +0800

    Python: Learn numpy index array

---
+++ python/learn/numpy/l2/idx_array.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+a = np.arange(10).reshape(2, 5)
+print(a)
+
+b = np.arange(100, 50, -2)
+print(b)
+
+#  c的形状和a相同
+c = b[a]
+print(c)
+
+d = b.reshape(-1, 5)
+print(d)
+
+f = np.array([True, False, True, False, True])
+print(f)
+
+#  f作为第一维度选在数组
+g = d[f, 0]
+print(g)
+
+h = d[0]
+print(h)
+
+i = d[0][0]
+print(i)
