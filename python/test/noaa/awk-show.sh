6a87b21d820b248853ae934ceda40fa8cc6943ed
Li Dong <lidong8@le.com>
2017-06-13 15:09:54 +0800

    Python: Test: 使用Ftp多线程下载气象局1901到当前的所有天气信息

---
+++ python/test/noaa/awk-show.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for year in out/1901/*
+do
+    echo -ne `basename $year .gz` "\t"
+    gunzip -c $year | \
+        awk '{ temp = substr($0, 88, 5) + 0;
+                q = substr($0, 93, 1);
+                if (temp != 9999 && q ~/[01459]/ && temp > max) max = temp }
+              END { print max }'
+done
