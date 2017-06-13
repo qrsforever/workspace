b835f716619a6499ff76f2a7a6f26a062812bf9b
Li Dong <lidong8@le.com>
2017-06-13 21:30:25 +0800

    Java: Learn: Hadoop权威指南起步

---
+++ java/learn/hadoop/guide/ch02/awk/cal.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+data_file=../bj_data.txt
+
+cat $data_file | \
+    awk '{
+            temp = substr($0, 88, 5) + 0;
+            q = substr($0, 93, 1);
+            if (temp != 9999 && q ~/[01459]/ && temp > max) {
+                max = temp 
+                datetime = substr($0, 16, 12)
+            }
+        }
+    END { print datetime, max }'
+
+# result:
+# 201007050630 410
+
---
+++ java/learn/hadoop/guide/ch02/gen_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+factory_dir=../../../../../python/test/noaa/
+
+if [[ ! -d $factory_dir/bj ]]
+then
+    echo "Error, First run below:"
+    echo -e "\t1. cd $factory_dir"
+    echo -e "\t2. ./get_beijing_weather.py"
+    exit 0
+fi
+
+start_year=2000
+
+for year in `ls $factory_dir/bj`
+do
+    if (( $year >= $start_year ))
+    then
+        gunzip -c $factory_dir/bj/$year/*.gz >> bj_data.txt
+    fi
+done
