d68af59b1d480f6c20b42c76ef8d514547732339
Li Dong <lidong8@le.com>
2017-06-13 17:13:38 +0800

    Python: Test: 添加只获取北京天气

---
+++ python/test/noaa/get_beijing_weather.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+下载国家气象局所有气候数据 (VPN连接)
+"""
+
+import os
+import re
+import threading
+from ftplib import FTP  
+from time import sleep
+
+def ftp_get_data(n, step):
+    # year: 1901 - 2017
+    #  pattern = r"[12][09][0-9]{2}"
+    start = n
+    end = n + step - 1
+    if n == 1:
+        start = 0
+    pattern = "[12][09][{}-{}][0-9]".format(start, end)
+    print(pattern)
+    match_year = re.compile(pattern)
+    
+    # 下载路径
+    # 采集站点编号: ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-history.txt
+    output = "bj"
+    bj_id = "545110" # USAF
+    if not os.path.exists(output):
+        os.mkdir(output)
+    else:
+        print("{} is exists".format(output))
+    
+    while True:
+        try: 
+            with FTP() as ftp:
+                ftp.set_debuglevel(2)
+                ftp.connect("ftp.ncdc.noaa.gov", 21, 60)
+                ftp.login() # 匿名登录(user=anonymous, passwd='')
+                ftp.getwelcome()
+                ftp.cwd("pub/data/noaa/")
+                ftp.set_debuglevel(0)
+                
+                files = ftp.nlst()
+                for name in files:
+                    result = match_year.match(name)
+                    if result is not None:
+                        for gzfile in ftp.nlst(name):
+                            print(gzfile)
+                            ret = re.search(bj_id, gzfile)
+                            if ret is None:
+                                continue
+                            year_dir = output + "/" + name
+                            if not os.path.exists(year_dir):
+                                os.mkdir(year_dir)
+                            print("Downloading ", gzfile)
+                            outfile = output + "/" + gzfile
+                            if os.path.exists(outfile):
+                                continue
+                            with open(outfile, 'wb') as f:
+                                ftp.retrbinary("RETR " + gzfile, f.write, 2048)
+                # 下载气候文件格式说明文档
+                formatdoc = "ish-format-document.pdf"
+                doc = output + formatdoc
+                if not os.path.exists(doc):
+                   with open(doc) as f:
+                       ftp.retrbinary("RETR " + formatdoc, f.write, 1024)
+                break
+        except Exception as err:
+            print(err)
+            sleep(3)
+
+
+if __name__ == "__main__":
+    # ftp 服务器最大允许2个线程访问
+    threads = []
+    step = 5
+    nloops = range(0, 9, step)
+    for i in nloops:
+       t = threading.Thread(target=ftp_get_data, args = (i, step))
+       threads.append(t)
+    for t in threads:
+       t.start()
+    for t in threads:
+       t.join()
+
