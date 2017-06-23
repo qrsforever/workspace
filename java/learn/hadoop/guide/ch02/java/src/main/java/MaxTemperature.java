66f7d4ac2585bb73e819096016951746b0598868
Li Dong <lidong8@le.com>
2017-06-23 11:45:38 +0800

    调整目录结构

--- java/learn/hadoop/guide/ch02/java/src/main/java/MaxTemperature.java
+++ java/learn/hadoop/guide/ch02/java/src/main/java/MaxTemperature.java
@@ -1,5 +1,3 @@
-// cc MaxTemperature Application to find the maximum temperature in the weather dataset
-// vv MaxTemperature
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
@@ -9,26 +7,33 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class MaxTemperature {
 
-  public static void main(String[] args) throws Exception {
-    if (args.length != 2) {
-      System.err.println("Usage: MaxTemperature <input path> <output path>");
-      System.exit(-1);
+    public static void main(String[] args) throws Exception {
+        if (args.length != 2) {
+            System.err.println("Usage: MaxTemperature <input path> <output path>");
+            System.exit(-1);
+        }
+
+        Job job = new Job();
+        job.setJarByClass(MaxTemperature.class);
+        job.setJobName("Max temperature");
+
+        // 输入/输出文件
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        // 设置map/reduder的处理类
+        job.setMapperClass(MaxTemperatureMapper.class);
+        job.setReducerClass(MaxTemperatureReducer.class);
+
+        // 设置map函数输出类型, 如果不设置就和reduce一致
+        // job.setMapOutputKeyClass(xx);
+        // job.setMapOutputValueClass(xx);
+
+        // 设置reduce函数输出类型
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        // 等待结果
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
-    
-    Job job = new Job();
-    job.setJarByClass(MaxTemperature.class);
-    job.setJobName("Max temperature");
-
-    FileInputFormat.addInputPath(job, new Path(args[0]));
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
-    
-    job.setMapperClass(MaxTemperatureMapper.class);
-    job.setReducerClass(MaxTemperatureReducer.class);
-
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(IntWritable.class);
-    
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
-  }
 }
-// ^^ MaxTemperature
--- java/learn/hadoop/guide/ch02/java/src/main/java/MaxTemperatureReducer.java
+++ java/learn/hadoop/guide/ch02/java/src/main/java/MaxTemperatureReducer.java
@@ -1,5 +1,3 @@
-// cc MaxTemperatureReducer Reducer for maximum temperature example
-// vv MaxTemperatureReducer
 import java.io.IOException;
 
 import org.apache.hadoop.io.IntWritable;
@@ -7,18 +5,17 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class MaxTemperatureReducer
-  extends Reducer<Text, IntWritable, Text, IntWritable> {
-  
-  @Override
-  public void reduce(Text key, Iterable<IntWritable> values,
-      Context context)
-      throws IOException, InterruptedException {
-    
-    int maxValue = Integer.MIN_VALUE;
-    for (IntWritable value : values) {
-      maxValue = Math.max(maxValue, value.get());
+    extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values,
+            Context context)
+        throws IOException, InterruptedException {
+
+        int maxValue = Integer.MIN_VALUE;
+        for (IntWritable value : values) {
+            maxValue = Math.max(maxValue, value.get());
+        }
+        context.write(key, new IntWritable(maxValue));
     }
-    context.write(key, new IntWritable(maxValue));
-  }
 }
-// ^^ MaxTemperatureReducer
