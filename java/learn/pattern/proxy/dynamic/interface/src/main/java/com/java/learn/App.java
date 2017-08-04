6a8463274e74d871cbb31fa0a1f5b73fd1f51280
Li Dong <lidong8@le.com>
2017-08-04 10:18:46 +0800

    Java:Learn:去掉每行的空格

--- java/learn/pattern/proxy/dynamic/interface/src/main/java/com/java/learn/App.java
+++ java/learn/pattern/proxy/dynamic/interface/src/main/java/com/java/learn/App.java
@@ -5,28 +5,27 @@ import java.lang.reflect.Proxy;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.class.path"));
 
         LeecoTV tv = new LeecoTV();
         // 1
         ClassLoader cl = tv.getClass().getClassLoader();
         // 2
         Class<?>[] ifaces = tv.getClass().getInterfaces();
-        // 3 
+        // 3
         InvocationHandler handle = new InvocationHandlerImpl(tv);
         // 4
         Object proxy = Proxy.newProxyInstance(cl, ifaces, handle);
         // 5
         ((INetwork)proxy).connect();
         ((IScreen)proxy).display();
-                          
+
         // 将生成的字节码输出到文件中
-        ProxyUtils.generateClassFile(tv.getClass(), "LeecoTVProxy");  
-    }                     
-}                         
-                          
-/*                        
- *                        
+        ProxyUtils.generateClassFile(tv.getClass(), "LeecoTVProxy");
+    }
+}
+
+/*
+ *
  *                    +---------------------------------------------------------------------+
  *                    |                                                                     |
  *             +------v----+         +-----------+                                          |
@@ -52,7 +51,7 @@ public class App {
  *                                              |              |   LeecoTVProxy    | ---+   |
  *                                      +-------V----+         |                   | -------+
  *                                      |            |         +-------------------+  implement
- *                                      |    Proxy   |                               
- *                                      |            |                               
- *                                      +------------+                               
- * */                                                                                  
+ *                                      |    Proxy   |
+ *                                      |            |
+ *                                      +------------+
+ * */
--- java/learn/pattern/proxy/dynamic/interface/src/main/java/com/java/learn/ProxyUtils.java
+++ java/learn/pattern/proxy/dynamic/interface/src/main/java/com/java/learn/ProxyUtils.java
@@ -1,8 +1,8 @@
 package com.java.learn;
 
-import java.io.FileOutputStream;  
-import java.io.IOException;  
-import sun.misc.ProxyGenerator;  
+import java.io.FileOutputStream;
+import java.io.IOException;
+import sun.misc.ProxyGenerator;
 
 public class ProxyUtils {
 
@@ -13,25 +13,25 @@ public class ProxyUtils {
      * @param proxyName 代理类的名字
      */
     public static void generateClassFile(Class<?> clazz, String proxyName) {
-        byte[] classFile = ProxyGenerator.generateProxyClass(proxyName, clazz.getInterfaces()); 
+        byte[] classFile = ProxyGenerator.generateProxyClass(proxyName, clazz.getInterfaces());
         String paths = clazz.getResource(".").getPath();
 
-        System.out.println(paths);  
-        FileOutputStream out = null;    
+        System.out.println(paths);
+        FileOutputStream out = null;
 
-        try {  
-            //保留到硬盘中  
-            out = new FileOutputStream(paths + proxyName + ".class");    
-            out.write(classFile);    
-            out.flush();    
-        } catch (Exception e) {    
-            e.printStackTrace();    
-        } finally {    
-            try {    
-                out.close();    
-            } catch (IOException e) {    
-                e.printStackTrace();    
-            }    
-        }    
-    }  
+        try {
+            //保留到硬盘中
+            out = new FileOutputStream(paths + proxyName + ".class");
+            out.write(classFile);
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
