a9d62055c1852d6f0b4877a278d34da0b15660c7
Li Dong <lidong8@le.com>
2017-09-11 20:11:35 +0800

    Python: Learn: Twisted inlineCallbacks

--- python/learn/twisted/inlineCallback.py
+++ python/learn/twisted/inlineCallback.py
@@ -4,14 +4,13 @@
 from twisted.internet import defer, reactor 
   
 def loadRemoteData(callback):  
-    print("-----------> loadRemoteData callback: ", callback)
+    print("---> loadRemoteData  callback: ", callback)
     import time 
     time.sleep(1)  
     callback(1) # 将1传给getResult, 只有callback之后才能触发callbacks结果
-    print("callback end!")
   
 def loadRemoteData2(callback):  
-    print("-----------> loadRemoteData2 callback: ", callback)
+    print("---> loadRemoteData2 callback: ", callback)
     import time  
     time.sleep(1)  
     callback(2)  
@@ -27,17 +26,19 @@ def getRemoteData():
     reactor.callInThread(loadRemoteData2, d2.callback)  
     print("yiled d2: ", d2)
     r2 = yield d2  
-  
-    defer.returnValue(r1+r2)
+    
+    # 不像其他生成器函数, send抛StopIteration, 异常, 而是主动跑_DefGen_Return异常
+    defer.returnValue(r1+r2) # raise
+    # 也可以使用return
+    #  return r1+r2
   
 def getResult(v):  
     print ("result = ", v)  
 
 if __name__ == '__main__':  
-    d = getRemoteData()  
-    print("main d = ", d)
-    print("main getResult: ", getResult)
-    d.addCallback(getResult)  
+    d0 = getRemoteData()  
+    print("main d0 : ", d0)
+    d0.addCallback(getResult)  
   
     #  import time 
     #  time.sleep(4)
