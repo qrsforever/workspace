9d91decd7ea774830492f72042eee70f7d0625ad
Li Dong <lidong8@le.com>
2018-06-06 18:01:31 +0800

    cpp:learn:functional,shared_ptr

--- cpp/learn/base/functional/src/test.cpp
+++ cpp/learn/base/functional/src/test.cpp
@@ -8,12 +8,20 @@
 
 #include <stdexcept>
 #include <functional>
+#include <memory>
 #include <cstdio>
+#include <map>
 
-template<typename R = void, typename... Args>
+template<typename R, typename... Args>
 class Functor {
 public:
     Functor(std::function<R(Args...)> fun) : _fun(fun) {
+
+    }
+
+    template<typename Object>
+    Functor(Object* object, R (Object::*method)(Args...))
+        : _fun([object, method](Args... args){ return (*object.*method)(args...);}) {
 
     }
 
@@ -25,18 +33,22 @@ private:
 };
 
 template <typename T_return> inline char get_return_code() {
-    throw std::logic_error("clipsmm: Adding function with invalid return type");
+    throw std::logic_error("test: Adding function with invalid return type");
 }
 template <> inline char get_return_code<void>() { return 'v'; }
 template <> inline char get_return_code<int>()  { return 'i'; }
+template <> inline char get_return_code<std::string>() { return 's'; }
 
 template <typename T_return> inline char get_argument_code() {
-    throw std::logic_error("clipsmm: Adding function with invalid argument type");
+    throw std::logic_error("test: Adding function with invalid argument type");
 }
 template <> inline char get_argument_code<int>() { return 'i'; }
+template <> inline char get_argument_code<char>() { return 'c'; }
 template <> inline char get_argument_code<std::string>() { return 's'; }
 
 void* gCB = 0;
+
+// std::map<std::string, auto> gFuns;
 
 template < typename T_return >
 bool add_function(std::string name, Functor<T_return> *call)
@@ -59,6 +71,20 @@ bool add_function(std::string name, Functor<T_return, T_arg1> *call)
     return true;
 }
 
+template < typename T_return, typename T_arg1, typename T_arg2>
+bool add_function(std::string name, std::shared_ptr<Functor<T_return, T_arg1, T_arg2>> call)
+{
+    printf("name[%s], get_return_code[%c] arg1[%c] arg2[%c]\n", name.c_str(),
+        get_return_code<T_return>(),
+        get_argument_code<T_arg1>(),
+        get_argument_code<T_arg2>());
+
+    printf("call count[%d]\n", call.use_count());
+
+    printf("\t call(100, 'x') = %d\n", (*call)(100, 'x'));
+    return true;
+}
+
 int test1()
 {
     printf("test1 return\n");
@@ -70,13 +96,45 @@ void test2(int a)
     printf("test2 a[%d]\n", a);
 }
 
+int test3(int a, char b)
+{
+    printf("test3 a[%d] b[%c]\n", a, b);
+    return 989;
+}
+
+class Test {
+public:
+    Test() {}
+    ~Test() {}
+    std::string test4(int a) {
+        printf("test4 a[%d]\n", a);
+        return "helloworld";
+    }
+};
+
 int main(void)
 {
+    /*
+     * 测试模板
+     * */
     add_function("test1", new Functor<int>(test1));
     add_function("test2", new Functor<void, int>(test2));
 
+    /*
+     * 测试类型转换
+     * */
     Functor<void, int> call = *(static_cast<Functor<void, int>*>(gCB));
     call(100);
 
+    /*
+     * 测试智能指针
+     */
+    add_function("test3", std::make_shared<Functor<int, int, char>>(test3));
+
+    /*
+     * 测试使用对象中的方法
+     */
+    Test *t = new Test();
+    add_function("test4", new Functor<std::string, int>(t, &Test::test4));
     return 0;
 }
--- cpp/learn/base/shared_ptr/test.cpp
+++ cpp/learn/base/shared_ptr/test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <memory>
+#include <cstdio>
 
 
 class Object {
@@ -38,14 +39,14 @@ void test(std::shared_ptr<Object> o)
 
 void run()
 {
-    // std::shared_ptr<StringData> data(std::make_shared<StringData>()); 
-    auto data(std::make_shared<StringData>()); 
+    // std::shared_ptr<StringData> data(std::make_shared<StringData>());
+    auto data(std::make_shared<StringData>());
     test(data);
     std::cout << "run end!" << std::endl;
 }
 
 struct BaseClass {};
- 
+
 struct DerivedClass : BaseClass
 {
     void f() const
@@ -56,20 +57,43 @@ struct DerivedClass : BaseClass
         std::cout << "~DerivedClass\n";
     }
 };
- 
+
+void test1(void *bar)
+{
+    std::shared_ptr<DerivedClass> sptr = *(std::shared_ptr<DerivedClass>*)(bar);
+    /* = 号触发引用+1 */
+    printf("sptr count %d, p = %p\n", sptr.use_count(), sptr.get());
+}
+
 int main(int argc, char *argv[])
 {
     run();
-    
+
     // std::shared_ptr<BaseClass> ptr_to_base(std::make_shared<DerivedClass>());
- 
+
     // // ptr_to_base->f(); // 错误不会编译： BaseClass 无名为 'f' 的成员
- 
+
     // std::static_pointer_cast<DerivedClass>(ptr_to_base)->f(); // OK
     // // （构造临时 shared_ptr ，然后调用 operator-> ）
- 
+
     // static_cast<DerivedClass*>(ptr_to_base.get())->f(); // 亦 OK
     // // （直接转型，不构造临时 shared_ptr ）
+
+
+    /*
+     * 测试 void* 转换
+     *
+     */
+    std::shared_ptr<DerivedClass> sptr = std::make_shared<DerivedClass>();
+    printf("sptr count %d p = %p\n", sptr.use_count(), sptr.get());
+    /* 引用没有变化 */
+    void * p1 = (void*)&sptr;
+    void * p2 = (void*)&sptr;
+    printf("sptr count %d p = %p\n", sptr.use_count(), sptr.get());
+    test1((void*)&sptr); /* 在转换之前必须要sptr, 全局保存一下, 谁也不知道test1会怎样 */
+
+    while (1);
+
     return 0;
 }
 
