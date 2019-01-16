b36cf6de68b2300df8cc7590c4ac265f3cef9d73
Li Dong <lidong8@le.com>
2019-01-16 18:39:20 +0800

    shell:test:apt

---
+++ shell/test/install_software/apt-get-tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install -y ssh
+
+sudo apt-get install -y gnome-core fcitx-bin fcitx-table gnome-tweak-tool
+
+sudo apt-get install -y git vim vim-gtk terminator chromium-browser goldendict
+
+sudo apt-get install -y xclip ctags cscope cmake cmake-curses-gui pkg-config scons
+
+sudo apt-get install -y python3-pip
