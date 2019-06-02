c564c960953e030cdedee60d8c6670c41f55fb61
Li Dong <lidong8@le.com>
2019-06-02 17:45:41 +0800

    ML:learn:snow

--- ML/learn/tensorflow/snowflake_neural_logic.py
+++ ML/learn/tensorflow/snowflake_neural_logic.py
@@ -18,10 +18,14 @@ import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
 import tensorflow as tf
+import memory_util
+import sys
 
 #####################################################################################
 # <codecell> global
 #####################################################################################
+
+memory_util.vlog(1)
 
 plt.rcParams['font.sans-serif'] = 'SimHei'
 plt.rcParams['axes.unicode_minus'] = False
@@ -46,6 +50,8 @@ r2 = r1/2 / np.cos(30*np.pi / 180)
 # 位移
 m = r1 + 5
 
+# 调试打印
+debug_ops = []
 xs = []
 ys = []
 
@@ -69,36 +75,33 @@ plt.xlim(0, 40)
 plt.ylim(0, 40)
 
 # 计算线性方程参数k,b,计算新点
-def plot_linear_equation(i, j, x1, x2):
+def plot_linear_equation(name, i, j, x1, x2):
     k = (ys[i] - ys[j]) / (xs[i] - xs[j])
     b = ys[i] - k * xs[i]
+    print('%s: (%d, %d) k = %.2f, b = %.2f' % (name, i, j, k, b))
     xp = (x1, x2)
     yp = (k*x1+b, k*x2+b)
     plt.plot(xp, yp, color='g')
-    plt.text(xp[1], yp[1], s = r'$y = %.2f %s %.2f$' % (k, '' if b < 0 else '+', b))
+    plt.text(xp[1], yp[1], s = r'%s: $y = %.2f %s %.2f$' % (
+        name, k, '' if b < 0 else '+', b))
     w = tf.constant((-1*k, 1), dtype=tf.float32)
-    w = tf.Print(w, [w], message="11")
+    debug_ops.append(tf.print('%s: ' % name, w, b+tf.math.reduce_sum(w * inputs),
+        output_stream=sys.stderr))
     return tf.cast(tf.nn.sigmoid(
-        tf.math.reduce_sum(w * inputs + b)) > 0.5, tf.int32)
+        tf.math.reduce_sum(w * inputs) + b) > 0.5, tf.int32)
 
-l1 = plot_linear_equation(3, 7, 4, 17)
-l2 = plot_linear_equation(1, 9, 13, 26)
-l3 = plot_linear_equation(5, 1, 4, 27)
-l4 = plot_linear_equation(7, 11, 4, 27)
-l5 = plot_linear_equation(3, 11, 13, 25)
-l6 = plot_linear_equation(5, 9, 4, 17)
+l1 = plot_linear_equation('l1', 3, 7, 4, 17)
+l2 = plot_linear_equation('l2', 1, 9, 13, 26)
+l3 = plot_linear_equation('l3', 5, 1, 4, 27)
+l4 = plot_linear_equation('l4', 7, 11, 4, 27)
+l5 = plot_linear_equation('l5', 3, 11, 13, 25)
+l6 = plot_linear_equation('l6', 5, 9, 4, 17)
 
-data = tf.stack([l1, l2, l3, l4, l5, l6])
-
-# l1.eval(feed_dict={inputs:[15, 15]})
-# l2.eval(feed_dict={inputs:[15, 15]})
-# l3.eval(feed_dict={inputs:[15, 15]})
-# l4.eval(feed_dict={inputs:[15, 15]})
-
-data.eval(feed_dict={inputs:[1, 15]})
+with tf.control_dependencies(debug_ops):
+    data = tf.stack([l1, l2, l3, l4, l5, l6])
+    with memory_util.capture_stderr() as stderr:
+        print(data.eval(feed_dict={inputs:[15, 15]}))
+        # l1.eval(feed_dict={inputs:[1, 15]})
+        print(stderr.getvalue())
 
 # sess.close() # noqa
-
-a = tf.constant([1.0, 3.0])
-a = tf.Print(a, [a], message="This is a: ")
-b = tf.add(a, a).eval()
