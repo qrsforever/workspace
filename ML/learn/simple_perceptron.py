706b2f8691ad352a76bf664cbefc1ae1a8bc3c0d
Li Dong <lidong8@le.com>
2019-06-04 20:56:04 +0800

    ML:learn:intro

--- ML/learn/simple_perceptron.py
+++ ML/learn/simple_perceptron.py
@@ -13,7 +13,19 @@
 # %pylab --no-import-all # noqa
 #####################################################################################
 
+import matplotlib.pylab as plt
 import numpy as np
+
+#####################################################################################
+# <codecell> global
+#####################################################################################
+
+plt.rcParams['font.sans-serif'] = 'SimHei'
+plt.rcParams['axes.unicode_minus'] = False
+plt.rcParams['figure.figsize'] = (16.0, 8.0)
+plt.rcParams['text.usetex'] = True
+plt.rcParams['text.latex.preamble'] = [r'\usepackage{amsmath}']
+plt.grid(True)
 
 #####################################################################################
 # <codecell>
@@ -61,7 +73,10 @@ import numpy as np
 #######################################################################################
 
 
-#
+#####################################################################################
+# <codecell> 单样本量
+#####################################################################################
+
 X = np.array([[1, 0, -1]]) # shape: (1, 3)
 W = np.array([[-5], [0], [1]])  # shape: (3, 1)
 
@@ -79,3 +94,87 @@ for i in range(150):
     else:
         print('finished')
         break
+
+#####################################################################################
+# <codecell> 多个样本量
+#####################################################################################
+
+X = np.array([
+    [1, 1, 1],
+    [1, 1, 3],
+    [1, 2, 1],
+    [1, 2, 3],
+    [1, 3, 2],
+    [1, 1, 4]])
+
+print(X[:, 0]) # this is bias
+
+W = np.array([[3], [0], [1]]) # start values, the first elem is bias
+
+Y = np.array([[1], [1], [1], [-1], [-1], [-1]])
+
+def calculate_params(X, Y, W):
+    m = len(X)
+    lr = 0.05
+
+    for i in range(1000):
+        if i % 10 == 0:
+            print('%d\n' % i, W)
+        y = np.sign(np.dot(X, W))
+        if (y != Y).any(): # 如果模型计算的y和已知标签Y的值不一致,更新参数
+            W = W - lr * np.matmul(X.T, (y-Y)) / m # 矩阵计算, m是样本量
+        else:
+            print('finished')
+            break
+
+    # 模拟数据, 在平面中显示这条切线
+    xs = np.random.uniform(0, 4, 100)
+    ys = (-W[0] - xs*W[1]) / W[2]
+    return xs, ys
+
+
+#####################################################################################
+# <codecell> plot
+#####################################################################################
+
+xs, ys = calculate_params(X, Y, W)
+
+fig, ax = plt.subplots(1, 1, figsize=(7,5))
+ax.plot(xs, ys)
+
+# xs = X[:, 1]
+# ys = X[:, 2]
+
+xs1 = []
+ys1 = []
+
+xs2 = []
+ys2 = []
+
+for i, v in enumerate(Y.reshape((len(Y), ))):
+    print(i)
+    if v > 0:
+        xs1.append(X[i][1])
+        ys1.append(X[i][2])
+    else:
+        xs2.append(X[i][1])
+        ys2.append(X[i][2])
+
+
+ax.scatter(xs1, ys1, marker='+')
+ax.scatter(xs2, ys2, marker='o')
+
+#####################################################################################
+# <codecell> add one point
+#####################################################################################
+
+fig, ax = plt.subplots(1, 1, figsize=(7,5))
+ax.plot(xs, ys)
+
+# 添加一个样本
+X = np.vstack((X, [[1, 2.5, 1.4]]))
+Y = np.vstack((Y, [[1]]))
+
+xs, ys = calculate_params(X, Y, W)
+ax.plot(xs, ys, color='red')
+ax.scatter(X[-1][1], X[-1][2], marker='^', color='blue')
---
+++ python/project/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+# @file config.py
+# @brief
+# @author QRS
+# @blog qrsforever.github.io
+# @version 1.0
+# @date 2019-06-04 12:56:04
+
+
+import warnings
+warnings.filterwarnings("ignore")
